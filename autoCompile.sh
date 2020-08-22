5c892e23f64be0ccf185fd25d4bcf1df58959610
EuniceMadya <ysun9316@gmail.com>
2020-08-22 15:48:35 +1000

    refine compile script

--- autoCompile.sh
+++ autoCompile.sh
@@ -1,1 +1,24 @@
-javac -Xlint  CET-impl/src/Components/*.java CET-impl/src/util/dagGen/*.java CET-impl/src/util/ArrayQueue.java CET-impl/src/util/GraphProcessor.java CET-impl/src/util/GraphGenerator.java CET-impl/src/util/FileGraphParser.java CET-impl/src/util/GraphBuilder.java CET-impl/src/util/RandomTimeGenerator.java CET-impl/src/Traversal/*.java CET-impl/src/Main.java
+#Componenets
+Components=CET-impl/src/Components/*.java
+
+#utils
+DagGenTool=CET-impl/src/util/dagGen/*.java
+ArrayQueue=CET-impl/src/util/ArrayQueue.java
+Processor=CET-impl/src/util/GraphProcessor.java
+Generator=CET-impl/src/util/GraphGenerator.java
+FileParser=CET-impl/src/util/FileGraphParser.java
+GraphBuilder=CET-impl/src/util/GraphBuilder.java
+RandomTimeGen=CET-impl/src/util/RandomTimeGenerator.java
+
+Util="$DagGenTool $ArrayQueue $Processor $Generator $FileParser $GraphBuilder $RandomTimeGen"
+
+#Algos
+TraversalAlgos=CET-impl/src/Traversal/*.java
+
+#Main executor
+Executor=CET-impl/src/AlgoExecutor.java
+Main=CET-impl/src/Main.java
+
+
+
+javac $Components $Util $TraversalAlgos $Executor $Main -d out/
