a7990e38bcc3aa1cd5b20c43588c194bebe6ac46
EuniceMadya <ysun9316@gmail.com>
2020-09-26 02:52:49 +1000

    hope garbage collection works

--- CET-impl/src/Traversal/SeqHybridGraphTraversal.java
+++ CET-impl/src/Traversal/SeqHybridGraphTraversal.java
@@ -131,7 +131,7 @@ public class SeqHybridGraphTraversal extends GraphTraversal {
                 queue.offer(combo);
             }
             currentPaths = null;
-            System.gc();
+//            System.gc();
         }
     }
 
