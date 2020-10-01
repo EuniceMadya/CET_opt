3c8719e4b815b5896d6f22ad5b750790b9996466
EuniceMadya <ysun9316@gmail.com>
2020-10-01 13:52:49 +1000

    change to int array and see changes

---
+++ CET-impl/src/SimpleExperiment/SingleLeveling.java
@@ -0,0 +1,4 @@
+package SimpleExperiment;
+
+public class SingleLeveling {
+}
--- CET-impl/src/util/IntArray.java
+++ CET-impl/src/util/IntArray.java
@@ -2,7 +2,7 @@ package util;
 
 import java.util.Arrays;
 
-public class IntArray{
+public class IntArray {
 
     private int currentPos;
     private int[] queue;
@@ -14,8 +14,8 @@ public class IntArray{
         this(10);
     }
 
-    public IntArray(IntArray newArray){
-        queue= new int[newArray.size()  + 10];
+    public IntArray(IntArray newArray) {
+        queue = new int[newArray.size() + 10];
         Arrays.fill(queue, -1);
         System.arraycopy(newArray.getWholeArray(), 0, queue, 0, newArray.size());
         currentPos = newArray.size();
@@ -24,6 +24,7 @@ public class IntArray{
 
     /**
      * Constructs an arraylist of the specified size.
+     *
      * @param startSize initial capacity
      */
     public IntArray(int startSize) {
@@ -36,6 +37,7 @@ public class IntArray{
     /**
      * Returns size of the queue after startPos.
      * Note: Can't use ArrayList.size because it may contain elements before startPos.
+     *
      * @return Size of queue.
      */
     public int size() {
@@ -44,6 +46,7 @@ public class IntArray{
 
     /**
      * Checks if the queue is empty.
+     *
      * @return true if empty, false if else.
      */
     public boolean isEmpty() {
@@ -53,13 +56,14 @@ public class IntArray{
 
     /**
      * Converts queue to array.
+     *
      * @return Queue in array form.
      */
     public int[] getArray() {
         return Arrays.copyOfRange(queue, 0, currentPos);
     }
 
-    public int[] getWholeArray(){
+    public int[] getWholeArray() {
         return queue;
     }
 
@@ -77,7 +81,7 @@ public class IntArray{
      * Resizes the queue, but only carries over elements from before the start position.
      */
     private void resize() {
-        int[] newQueue = new int [(int) (queue.length * 1.75) + 1];
+        int[] newQueue = new int[(int) (queue.length * 1.75) + 1];
         Arrays.fill(newQueue, -1);
 
         System.arraycopy(queue, 0, newQueue, 0, queue.length);
@@ -88,6 +92,7 @@ public class IntArray{
 
     /**
      * Adds the object to the end of the queue. Throws NullPointerException if element is null.
+     *
      * @param i int to be added.
      */
     public void add(int i) {
@@ -102,15 +107,16 @@ public class IntArray{
 
     /**
      * Adds the object to the end of the queue. Throws NullPointerException if element is null.
+     *
      * @param pos int to get.
      */
     private int get(int pos) {
-        if(pos > currentPos ) throw new NumberFormatException();
+        if (pos > currentPos) throw new NumberFormatException();
 
         return queue[pos];
     }
 
-    public int getLast(){
+    public int getLast() {
         return queue[currentPos - 1];
     }
 
