5845eaa3a0da89c66e5f020ba43a4f9eb689b8c9
EuniceMadya <ysun9316@gmail.com>
2020-10-29 18:29:40 +1100

    update data

--- ResultProcessor/processor.py
+++ ResultProcessor/processor.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import os
+from itertools import takewhile
 
 TOTAL_NODE_NUM: int = 0
 MIN_ANCHOR_NUM: int = 10000
@@ -9,31 +10,33 @@ MAX_TIME: float = 0.0
 ALGORITHMS: dict = {"BFS": "BFS", "DFS": "DFS", "Anchor":"Anchor"}
 CONCATENATE: dict = {"BFS":"B", "DFS": "D"}
 LEVEL_NUM: dict = {"Single": "Single", "Double":"Double"}
-THREADING_TYPE: dict = {"Sequential": "Seq", "Concurrent": "Con"}
-SPLIT_TYPE: dict = {"LD": "LargestDegree", "Random": "Random", "Distro": "EquallyDistribute"}
+THREADING_TYPE: dict = {"Seq": "Sequential", "Con": "Concurrent"}
+SPLIT_TYPE: dict = {"LD": "LargestDegree", "Random": "Random", "EquallyDistribute": "EquallyDistribute"}
 
 #filename: nodenum-frequency-ALGORITHMS(-LEVEL_NUM-THREADING_TYPE-SPLIT_TYPE-DCONCATENATE).txt
 
 def list_options(num_nodes: int):
+    global RES
 
     file_list = os.listdir(str(num_nodes))
     option_string = "0. Exit\n"
-    options = []
+    prefix = []
     selections = []
+    selection_id = []
     actual_names = []
     counter = 1
     file_list.sort()
     for filename in file_list:
-        if filename.startswith("CSR"):
+        if (not filename.startswith(str(num_nodes))):
             continue
         filename.rstrip()
         actual_names.append(filename)
         filename = filename.replace(".txt", "")
         name = filename[len(str(num_nodes)) + 6 : ]
         option_string = option_string + str(counter) + ". "+ name + "\n"
-        options.append(name)
-
+        prefix.append(name)
         counter = counter + 1
+
 
     selection = "-1"
     while 1:
@@ -41,12 +44,31 @@ def list_options(num_nodes: int):
         print(option_string)
         selection = int(input())
         if selection == 0:
-            return
-        selections.append(options[selection - 1])
+            break
+        selections.append(prefix[selection - 1])
+        selection_id.append(selection)
         print("\nCurrent selections: ")
         print(selection_to_string(selections))
-        process_selection(actual_names[selection - 1], options[selection - 1])
 
+    selections_without_BD = selections
+    try:
+        selections_without_BD.remove("BFS")
+
+    except:
+        pass
+
+    try:
+        selections_without_BD.remove("DFS")
+    except Exception as e:
+        pass
+
+    res = get_common_prefix(selections_without_BD)
+
+    print("common prefix: ", res)
+
+    for id in selection_id:
+        remove_prefix = prefix[id-1].replace(res, "")
+        process_selection(actual_names[id - 1], remove_prefix)
 
 
 def selection_to_string(selections: list):
@@ -55,7 +77,6 @@ def selection_to_string(selections: list):
         string_output = string_output + selection + "\n"
 
     return string_output
-
 
 
 def process_selection(filename: str, name: str):
@@ -75,7 +96,7 @@ def add_DFSorBFS(algo: str, data: str):
     x = []
     y = []
     data_list = data.split(",")
-    for i in range(0, MAX_ANCHOR_NUM + 5, 5):
+    for i in range(0, TOTAL_NODE_NUM + 5, 5):
         x.append(i)
         y.append(float(data_list[0]))
         if (float(data_list[0]) > MAX_TIME):
@@ -107,12 +128,28 @@ def add_anchor(algo: str, data: list):
     plt.legend()
 
 
+def get_common_prefix(selections: list):
+    print("selections", selections)
+    res = ''.join(c[0] for c in takewhile(lambda x: all(x[0] == y for y in x), zip(*selections)))
+    print("啊啊啊啊啊", res)
+    common_attrs = ""
+    if "-" in res:
+        common_attrs = res[:res.rindex("-")]
+    return common_attrs
+
+
 TOTAL_NODE_NUM = int(input("Choose the graph you want to generate from: "))
 list_options(TOTAL_NODE_NUM)
 
+if(MIN_ANCHOR_NUM > MAX_ANCHOR_NUM):
+    MAX_ANCHOR_NUM = TOTAL_NODE_NUM
+    MIN_ANCHOR_NUM = 0
+
 plt.axis([MIN_ANCHOR_NUM - 5, MAX_ANCHOR_NUM + 5, 0, MAX_TIME + MAX_TIME/10])
 plt.grid(linestyle='--')
-plt.title("Algorithm comparison for " + str(TOTAL_NODE_NUM) + " nodes")
+
+
+plt.title("Algorithm comparison for " + str(TOTAL_NODE_NUM) + " nodes" )
 ax = plt.gca()
 ax.spines['top'].set_visible(False)
 ax.spines['right'].set_visible(False)
